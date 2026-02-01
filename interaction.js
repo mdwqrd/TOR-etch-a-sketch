304872686df4ef2e7bfa918943973eb228845f0b
mdwqrd <meddwqrd@gmail.com>
2026-02-01 13:00:57 +0300

    Added more functionality to js

--- interaction.js
+++ interaction.js
@@ -1,9 +1,16 @@
 const rowContainer = document.getElementById('canvas-row-container');
 const canvasContainer = document.querySelector("canvas");
-const colorSelector = document.getElementById('color-selector');
+const brushSelector = document.getElementById('brush-selector');
+const bgSelector = document.getElementById('canvas-selector');
 const gridSelector = document.getElementById('grid-selector');
 const applyButton = document.getElementById('apply');
 const resetButton = document.getElementById('reset');
+
+let bgColor = bgSelector.value;
+let brushColor = brushSelector.value;
+let grid = gridSelector.value;
+
+setupCanvas();
 
 //used to clear out the canvas from previous grid. Should be bound
 //to reset button event listener and grid update.
@@ -27,16 +34,53 @@ function fillCanvas(gridSize) {
 //prepares 'pixel div' to be added inside a pixel row in canvas
 function setupPixelDiv() {
     const pixelDiv = document.createElement('div');
-    pixelDiv.setAttribute("style", "background: white; flex: 1;");
+    pixelDiv.setAttribute("style", "flex: 1;");
+    pixelDiv.style.backgroundColor = bgColor;
     pixelDiv.addEventListener('mouseover', () => {
-        pixelDiv.style.backgroundColor = colorSelector.value;
+        pixelDiv.style.backgroundColor = brushSelector.value;
     });
     return pixelDiv;
 }
 
+function updateColors() {
+    bgColor = bgSelector.value;
+    brushColor = brushSelector.value;
+}
+
+function updateGrid() {
+    grid = gridSelector.value;
+}
+
+function updateSettings() {
+    updateColors();
+    updateGrid();
+}
+
 function setupCanvas() {
     clearCanvas();
-    fillCanvas(gridSelector.value);
+    updateSettings();
+    fillCanvas(grid);
+}
+
+function resetCanvas() {
+    clearCanvas();
+    fillCanvas(grid);
 }
 
 applyButton.addEventListener("click", setupCanvas);
+resetButton.addEventListener("click", resetCanvas);
+
+gridSelector.addEventListener("input", function(event) {
+    grid = event.target.value;
+    console.log("Grid value updated.");
+});
+
+brushSelector.addEventListener("input", function(event) {
+    brushColor = event.target.value;
+    console.log("Brush color updated.");
+});
+
+bgSelector.addEventListener("input", function(event) {
+    bgColor = event.target.value;
+    console.log("Background Color Changed.")
+});
